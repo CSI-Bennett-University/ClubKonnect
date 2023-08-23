0a7231f52d8125d740834b51b546cad635da619f
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-08-23 15:23:38 +0530

    production security

--- Recruitments/settings.py
+++ Recruitments/settings.py
@@ -27,6 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-e1!dlfy=m35ld&ia*ic_=p5toyx)u!x*%8n3opv(%(%8f)z6l_'
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 86400  # 1 day
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
